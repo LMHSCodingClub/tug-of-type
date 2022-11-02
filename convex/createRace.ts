f50715113fb528d8da1bb5f07a0fa61a1faf7a7a
jasonnagel268 <jason_nagel26@yahoo.com>
2022-11-02 16:04:36 -0400

    Worked on createRace

--- convex/createRace.ts
+++ convex/createRace.ts
@@ -2,8 +2,11 @@ import { mutation } from './_generated/server'
 
 export default mutation(
   async ({ db }, ul: string[], tmr: number, txt: string) => {
-
+    const totalRows = await db.query('texts').length
+    const rowNumber = Math.random() * ((totalRows)-1) + 1
     await db
+    .query('texts')[rowNumber].text
+    
       .insert('races', {
         userList: ul,
         timer: tmr,
--- convex/createUser.ts
+++ convex/createUser.ts
@@ -2,6 +2,9 @@ import { mutation } from './_generated/server'
 
 export default mutation(
   async ({ db }, un: string, pswd: string) => {
+    
+      )
+
     await db
       .insert ('users' , { 
             userName : un, password : pswd
