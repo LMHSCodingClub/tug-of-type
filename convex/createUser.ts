88abab749374b4f5bd1797a2cb25a3a9c63908a0
jasonnagel268 <jason_nagel26@yahoo.com>
2022-11-02 14:39:28 -0400

    implement createUser

---
+++ convex/createUser.ts
@@ -0,0 +1,11 @@
+import { mutation } from './_generated/server'
+
+export default mutation(
+  async ({ db }, un: string, pswd: string) => {
+    await db
+      .insert ('users' , { 
+            userName : un, password : pswd
+      })
+
+  }
+)
