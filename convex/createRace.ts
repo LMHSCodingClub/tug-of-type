1fc6d53837406dbd9b83406a18f6429caec3d053
ASJ5630 <67477846+ASJ5630@users.noreply.github.com>
2022-11-18 10:23:30 -0500

    added leaderboard

--- convex/createRace.ts
+++ convex/createRace.ts
@@ -2,7 +2,7 @@ import { mutation } from "./_generated/server"
 
 export default mutation(
   async ({ db }: any) => {
-    // TODO: Use Math.random() inside a filter to randomly return that text instead of two expensive queriesz
+    // TODO: Use Math.random() inside a filter to randomly return that text instead of two expensive queries
     const totalRows = (await db.query('texts').collect()).length;
     const rowNumber = Math.random() * ((totalRows) - 1) + 1;
     const txt = (await db.query('texts').collect())[rowNumber]
