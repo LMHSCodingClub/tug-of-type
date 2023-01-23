41c29f4b010b5684729338543d4259f28d97885b
Varun <31084854+varunsingh87@users.noreply.github.com>
2023-01-22 22:06:11 -0500

    Convert non-ASCII to ASCII when creating text

--- convex/createText.js
+++ convex/createText.js
@@ -2,6 +2,7 @@ import { mutation } from './_generated/server'
 
 export default mutation(
   async ({ db }, txt, scr) => {
+    txt = txt.replace(/[\u2019\u2018]/g, "'").replace(/\u201C\u201D\u201F/g, '"').replace(/\u2014\u2013\u2012/g);
     await db
       .insert('texts', {
         words: txt,
